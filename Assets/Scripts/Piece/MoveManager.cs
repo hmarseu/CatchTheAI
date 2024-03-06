feecc5bb5d9a23fd77bde75fa23f09b3648de10a
hmarseu <hmarseu@gaming.tech>
2024-03-06 10:38:32 +0100

    j'ai fais mon devoir

--- Assets/Scripts/Piece/MoveManager.cs
+++ Assets/Scripts/Piece/MoveManager.cs
@@ -6,7 +6,7 @@ public class MoveManager : MonoBehaviour
 {
     public delegate void Possibilities(List<Vector2Int> tabPossibilte);
     public static event Possibilities possibilities;
-
+    private GameObject[,] tempBoardArray;
     private void OnEnable()
     {
         BoardManager.transferPion += PossibleMove;
@@ -18,6 +18,7 @@ public class MoveManager : MonoBehaviour
     }
     private void PossibleMove(SOPiece so, Vector2Int position, GameObject[,] boardarray)
     {
+        tempBoardArray = boardarray;
         List<Vector2Int> validMoves = new List<Vector2Int>();
         /* 
          * quelle est mouvement peut faire la piece 
@@ -40,13 +41,13 @@ public class MoveManager : MonoBehaviour
         int[] deltaX = { -1,  0,  1, -1, 0, 1,-1, 0 ,1};
         int[] deltaY = { -1, -1, -1,  0, 0, 0, 1, 1 ,1};
         
-        for (int i = 0;i <= so.PossibleMoves.Count;i++)
+        for (int i = 0;i < so.PossibleMoves.Count;i++)
         {
             if (so.PossibleMoves[i])
             {
                 int newX = position.x + deltaX[i];
                 int newY = position.y + deltaY[i];
-                if (IsInsideBoard(newX,newY))
+                if (IsInsideBoard(newX,newY) && IsCaseEmpty(new Vector2Int(newX,newY)))
                 {
                     validMoves.Add(new Vector2Int(newX,newY));
                 }
@@ -60,5 +61,18 @@ public class MoveManager : MonoBehaviour
     private bool IsInsideBoard(int x, int y)
     {
         return x >= 0 && x < 5 && y >= 0 && y < 4; // 5 lignes (0 à 4) et 4 colonnes (0 à 3)
+    }
+
+    private bool IsCaseEmpty(Vector2Int pos)
+    {
+        //a ajouter la verification du "camp" de ce pion
+        if (tempBoardArray[pos.x, pos.y].transform.GetChild(0).gameObject)
+        {
+            // piece pion = tempBoardArray[pos.x, pos.y].transform.GetChild(0).gameObject.getcomponent<piece>()
+            //if   pion.player ==...
+            return false;
+        }
+       
+        return true;
     }
 }
